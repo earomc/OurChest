b7310ff479b7fa7f1c38237f8d08ab1253fae190
earomc <moritz.brunne@gmail.com>
2025-07-26 03:42:06 +0200

    Switch to extending BaseEntityBlock instead of AbstractChestBlock

--- src/main/java/net/earomc/ourchest/ourchest/OurChestBlock.java
+++ src/main/java/net/earomc/ourchest/ourchest/OurChestBlock.java
@@ -4,13 +4,13 @@ import com.mojang.serialization.MapCodec;
 import net.earomc.ourchest.ModBlockEntityTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.MenuProvider;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.AbstractChestBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DoubleBlockCombiner;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -20,15 +20,14 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
-public class OurChestBlock extends AbstractChestBlock<OurChestBlockEntity> {
+public class OurChestBlock extends BaseEntityBlock {
 
     private static final MapCodec<OurChestBlock> CODEC = simpleCodec(OurChestBlock::new);
     private static final VoxelShape SHAPE = Block.column(14.0, 0.0, 14.0);
     public static final EnumProperty<Direction> FACING = HorizontalDirectionalBlock.FACING;
 
-
     public OurChestBlock(BlockBehaviour.Properties properties) {
-        super(properties, () -> ModBlockEntityTypes.OUR_CHEST);
+        super(properties);
         this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
@@ -43,11 +42,6 @@ public class OurChestBlock extends AbstractChestBlock<OurChestBlockEntity> {
     }
 
     @Override
-    public DoubleBlockCombiner.NeighborCombineResult<? extends ChestBlockEntity> combine(BlockState blockState, Level level, BlockPos blockPos, boolean bl) {
-        return DoubleBlockCombiner.Combiner::acceptNone;
-    }
-
-    @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new OurChestBlockEntity(blockPos, blockState);
     }
@@ -55,5 +49,15 @@ public class OurChestBlock extends AbstractChestBlock<OurChestBlockEntity> {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(FACING);
+    }
+
+    @Override
+    public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> blockEntityType) {
+        return level.isClientSide ? createTickerHelper(blockEntityType, ModBlockEntityTypes.OUR_CHEST, OurChestBlockEntity::lidAnimateTick) : null;
+    }
+
+    @Override
+    protected @Nullable MenuProvider getMenuProvider(BlockState blockState, Level level, BlockPos blockPos) {
+        return super.getMenuProvider(blockState, level, blockPos);
     }
 }
